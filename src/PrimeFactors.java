9caa72d8c3fb2104d6974f120663775ce0abd731
dan-peveto <daniel_peveto@outlook.com>
2023-11-07 15:47:12 -0800

    testOne complete

---
+++ src/PrimeFactors.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * PrimeFactors
+ */
+public class PrimeFactors {
+    public List<Integer> resolve(int value) {
+        return new ArrayList<Integer>();
+    }
+    
+}
---
+++ src/PrimeFactorsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeFactorsTest {
+    @Test
+    public void testOne() {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> expected = new ArrayList<Integer>();
+        List<Integer> actual = primeFactors.resolve(1);
+        assertEquals(expected, actual);
+    }    
+}
