01e6d0cdba57afd452f9e1abeab88251706be624
dan-peveto <daniel_peveto@outlook.com>
2023-11-07 16:05:22 -0800

    refactoring added helper method listOf

--- src/PrimeFactorsTest.java
+++ src/PrimeFactorsTest.java
@@ -8,16 +8,22 @@ public class PrimeFactorsTest {
     @Test
     public void testOne() {
         PrimeFactors primeFactors = new PrimeFactors();
-        List<Integer> expected = new ArrayList<Integer>();
+        List<Integer> expected = listOf();
         List<Integer> actual = primeFactors.resolve(1);
         assertEquals(expected, actual);
     }    
     @Test
     public void testTwo() {
         PrimeFactors primeFactors = new PrimeFactors();
-        List<Integer> expected = new ArrayList<Integer>();
-        expected.add(2);
+        List<Integer> expected = listOf(2);
         List<Integer> actual = primeFactors.resolve(2);
         assertEquals(expected, actual);
+    }
+    private static List<Integer> listOf(int... values) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int value : values) {
+            result.add(value);
+        }
+        return result;
     }
 }
